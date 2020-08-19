cc986e37e0cfb1b86c7c763d151c9875c0ede1e9
Sergio Segaty <sergio.segaty@gmail.com>
2020-08-19 16:47:34 -0300

    Começando

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableNativeFeedback,
+  Platform,
+} from "react-native";
 
 export default function App() {
+
+  _onPressButton = () => {
+    alert('Apertou o botao!')
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome Sérgio, your friends suck cock </Text>
       <StatusBar style="auto" />
+      <TouchableNativeFeedback
+        onPress={this._onPressButton}
+        backgorund={
+          Platform.OS === "android"
+            ? TouchableNativeFeedback.SelectableBackground()
+            : ""
+        }
+      >
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>
+            Meu Botao
+            {Platform.OS !== "android" ? "(Android Only)" : ""}
+          </Text>
+        </View>
+      </TouchableNativeFeedback>
     </View>
   );
 }
@@ -14,8 +42,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  button: {
+    marginTop: 35,
+    marginBottom: 30,
+    width: 260,
+    alignItems: "center",
+    backgroundColor: "#2196F3",
+  },
+
+  buttonText: {
+    textAlign: "center",
+    padding: 20,
+    color: "white",
   },
 });
